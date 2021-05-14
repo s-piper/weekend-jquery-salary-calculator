8e4cbf5ae5fedab90ad3dbcd1415588d28db3574
Stephen Piper <stephen.r.piper@gmail.com>
2021-05-14 16:03:30 -0500

    created listener for submit button and addEmployee function

--- client.js
+++ client.js
@@ -5,4 +5,21 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('jquery loaded');
+
+    $('#submit').on('click', addEmployee);
+}
+
+function addEmployee() {
+    console.log('addEmployee click');
+    
+    let employee = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        id: $('#id').val(),
+        title: $('#title').val(),
+        annualSalary: $('#annualSalary').val(),
+    }
+
+    console.log(employee);
+    
 }
