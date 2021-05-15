b17992843d1f90b8d90f9ce356a17fc56b6b3eb9
Stephen Piper <stephen.r.piper@gmail.com>
2021-05-15 17:08:15 -0500

    created a total function that pulls salaries from array converts to numbers adds and updates DOM.  Need to put the math in to break it into monthly

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ console.log('js loaded');
 
 $(document).ready(readyNow);
 
-function readyNow(){
+function readyNow() {
     console.log('jquery loaded');
 
     $('#submit').on('click', addEmployee);
@@ -11,12 +11,13 @@ function readyNow(){
 
     $('.table').on('click', '.delete', deleteEmployee)
     //dynamic listener for delete button click on table
+
 }
 
 function addEmployee() {
     //start addEmployee function
     console.log('addEmployee click');
-    
+
     let employee = {
         firstName: $('#firstName').val(),
         lastName: $('#lastName').val(),
@@ -25,31 +26,59 @@ function addEmployee() {
         annualSalary: $('#annualSalary').val(),
     }
 
+    salaryArray.push(employee.annualSalary);
+    //pushes annualSalary to salaryArray to store 
+
+
     console.log(employee.firstName, employee.lastName);
-    
+
     //takes info from inputs an employee object
     $('.input').val('');
     //clears the inputs
 
     let newRow = `<tr><td>${employee.firstName}</td><td>${employee.lastName}</td>
-        <td>${employee.id}</td><td>${employee.title}</td><td style="text-align:right">${employee.annualSalary}
+        <td>${employee.id}</td><td>${employee.title}</td><td style="text-align:right">$${employee.annualSalary}
         <td style="text-align:center"><button class="delete">Delete</button></td>  </tr>`;
-        //creates new row pulling info from employee object
-        //also makes the delete button
+    //creates new row pulling info from employee object
+    //also makes the delete button
     let tableBody = $('table tbody');
 
     tableBody.append(newRow);
-        //adds new row to DOM
-    
+    //adds new row to DOM
+
     console.log(employee);
-    
+
+
+
+    //let total = employee.annualSalary;
+    //console.log(total);
+
+    total();
+    //calls total function
+
 }//end addEmployee
-
-
 
 
 function deleteEmployee() {
     console.log('delete click');
 
     $(this).closest(`tr`).remove();
-    }//deletes row
+}//deletes row
+
+
+let salaryArray = [];
+let annual = 0;
+// console.log(salaryArray.reduce((a, b) => a + b, 0));
+
+
+function total() {// total function start
+    
+    for (let i = 0; i < salaryArray.length; i++) {
+       salaryArray[i] = +salaryArray[i];   
+    }//converts the array from strings to numbers
+    console.log(salaryArray.reduce((a, b) => a + b, 0));
+   annual = salaryArray.reduce((a, b) => a + b, 0);
+   $('#total').text(`Total Monthly:$${annual}`);
+
+
+}//total function end
